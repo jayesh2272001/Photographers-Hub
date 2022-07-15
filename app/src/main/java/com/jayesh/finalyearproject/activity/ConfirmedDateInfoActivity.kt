c29d4df8358f3e99ca89c09b459cacd825ab9e09
Jayesh shinde <jay@shinde.com>
2022-07-15 09:33:23 +0530

    method 2 of payment gatway (failed)

--- app/src/main/java/com/jayesh/finalyearproject/activity/ConfirmedDateInfoActivity.kt
+++ app/src/main/java/com/jayesh/finalyearproject/activity/ConfirmedDateInfoActivity.kt
@@ -1,6 +1,7 @@
 package com.jayesh.finalyearproject.activity
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
@@ -16,8 +17,10 @@ import com.jayesh.finalyearproject.R
 import com.jayesh.finalyearproject.model.UPI
 import dev.shreyaspatil.easyupipayment.EasyUpiPayment
 import dev.shreyaspatil.easyupipayment.listener.PaymentStatusListener
+import dev.shreyaspatil.easyupipayment.model.PaymentApp
 import dev.shreyaspatil.easyupipayment.model.TransactionDetails
 import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.random.Random
 import kotlin.random.Random.Default.nextInt
 
@@ -36,7 +39,7 @@ class ConfirmedDateInfoActivity : AppCompatActivity() {
     private lateinit var btnCheckProfile: Button
     private lateinit var btnProceedToPay: Button
     private lateinit var tvPaymentInfo: TextView
-
+    val UPI_PAYMENT = 100
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -91,61 +94,44 @@ class ConfirmedDateInfoActivity : AppCompatActivity() {
                         val am = payableAmount?.toFloat()
                         Log.i("UPI", "amount $am")
 
-                        val easyUpiPayment = EasyUpiPayment(this@ConfirmedDateInfoActivity) {
-                            this.payeeVpa = "ksaiee263@okhdfcbank"
-//                            this.payeeVpa = upi.toString()
-                            this.payeeName = pName
-                            this.payeeMerchantCode = "1823"
-                            this.transactionId = "T$transId"
-                            this.transactionRefId = "T$transId"
-                            this.description = "Payment to $pName for Photographers hub"
-                            this.amount = "1.00"
-//                            this.amount = am.toString()
-                        }
-                        easyUpiPayment.startPayment()
-                        easyUpiPayment.setPaymentStatusListener(
-                            object : PaymentStatusListener {
-                                override fun onTransactionCancelled() {
-                                    Toast.makeText(
-                                        this@ConfirmedDateInfoActivity,
-                                        "Transaction is cancelled",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                }
+                        //upi starts
 
-                                override fun onTransactionCompleted(transactionDetails: TransactionDetails) {
-                                    Log.i("Transaction details", transactionDetails.toString())
-                                    Toast.makeText(
-                                        this@ConfirmedDateInfoActivity,
-                                        "$transactionDetails",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                }
+                        if (am.toString().length > 0) {
+                            var uri = Uri.parse("upi://pay").buildUpon()
+                                .appendQueryParameter("pa", "9763570430@postbank")
+                                .appendQueryParameter("mc", "")
+                                .appendQueryParameter("tid", "t$transId")
+                                .appendQueryParameter("tr", "tr$transId")
+//                                .appendQueryParameter("pa", upi)
+                                .appendQueryParameter("pn", pName)
+                                .appendQueryParameter("tn", "pay to $pName")
+                                .appendQueryParameter("am", "1.0")
+                                .appendQueryParameter("cu", "INR")
+                                .build();
 
+                            var intent = Intent(Intent.ACTION_VIEW);
+                            intent.data = uri
+                            var intentChooser = Intent.createChooser(intent, "Pay with")
 
+                            if (null != intentChooser.resolveActivity(getPackageManager())) {
+                                startActivityForResult(intentChooser, UPI_PAYMENT);
+                            } else {
+                                Toast.makeText(
+                                    this@ConfirmedDateInfoActivity,
+                                    "No UPI app found, please install one to continue",
+                                    Toast.LENGTH_SHORT
+                                ).show();
                             }
-                        )
 
+                        } else {
+                            Toast.makeText(
+                                this@ConfirmedDateInfoActivity,
+                                "Please enter amount",
+                                Toast.LENGTH_SHORT
+                            ).show();
 
-                        /*google pay integration starts*/
-
-                        /* val uri: Uri = Uri.Builder()
-                             .scheme("upi")
-                             .authority("pay")
-                             .appendQueryParameter("pa", upi.toString())
-                             .appendQueryParameter("pn", pName)
-                             //.appendQueryParameter("mc", "your-merchant-code")
-                             //.appendQueryParameter("tr", "your-transaction-ref-id")
-                             .appendQueryParameter("tn", "Payment to $pName")
-                             .appendQueryParameter("am", "01")//change amount later
-                             .appendQueryParameter("cu", "INR")
-                             //.appendQueryParameter("url", "your-transaction-url")
-                             .build()
-                         val intent = Intent(Intent.ACTION_VIEW)
-                         intent.data = uri
-                         intent.setPackage(GOOGLE_PAY_PACKAGE_NAME)
-                         startActivityForResult(intent, GOOGLE_PAY_REQUEST_CODE)*/
-                        /*google pay integration ends*/
+                        }
+                        //upi ends
 
 
                     }
@@ -153,102 +139,10 @@ class ConfirmedDateInfoActivity : AppCompatActivity() {
                     override fun onCancelled(error: DatabaseError) {
                         Log.i("error", error.toString())
                     }
-
                 })
-
-
         }
 
     }
-
-
-/*
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        Log.e("main ", "response $resultCode")
-        if (resultCode == GOOGLE_PAY_REQUEST_CODE) {
-            if ((RESULT_OK == resultCode) || (resultCode == 11)) {
-                if (data != null) {
-                    val trxt: String = data.getStringExtra("response")!!
-                    Log.e("UPI", "onActivityResult: $trxt")
-                    val dataList: ArrayList<String> = ArrayList()
-                    dataList.add(trxt)
-                    upiPaymentDataOperation(dataList)
-                } else {
-                    Log.e("UPI", "onActivityResult: " + "Return data is null")
-                    val dataList: ArrayList<String> = ArrayList()
-                    dataList.add("nothing")
-                    upiPaymentDataOperation(dataList)
-                }
-            } else {
-                //when user simply back without payment
-
-                //when user simply back without payment
-                Log.e("UPI", "onActivityResult: " + "Return data is null")
-                val dataList: ArrayList<String> = ArrayList()
-                dataList.add("nothing")
-                upiPaymentDataOperation(dataList)
-            }
-        }
-
-    }
-*/
-
-/*
-    private fun upiPaymentDataOperation(data: ArrayList<String>) {
-
-        if (isConnectionAvailable(this)) {
-            var str: String = data[0]
-            Log.e("UPIPAY", "upiPaymentDataOperation: $str")
-            var paymentCancel = ""
-            if (str == null) str = "discard"
-            var status = ""
-            var approvalRefNo = ""
-            val response = str.split("&").toTypedArray()
-            for (i in response.indices) {
-                val equalStr = response[i].split("=").toTypedArray()
-                if (equalStr.size >= 2) {
-                    if (equalStr[0].lowercase(Locale.getDefault()) == "Status".lowercase(Locale.getDefault())) {
-                        status = equalStr[1].lowercase(Locale.getDefault())
-                    } else if (equalStr[0].lowercase(Locale.getDefault()) == "ApprovalRefNo".lowercase(
-                            Locale.getDefault()
-                        ) || equalStr[0].lowercase(Locale.getDefault()) == "txnRef".lowercase(
-                            Locale.getDefault()
-                        )
-                    ) {
-                        approvalRefNo = equalStr[1]
-                    }
-                } else {
-                    paymentCancel = "Payment cancelled by user."
-                }
-            }
-            if (status == "success") {
-                //Code to handle successful transaction here.
-                Toast.makeText(this, "Transaction successful.", Toast.LENGTH_SHORT)
-                    .show()
-                Log.e("UPI", "payment successful: $approvalRefNo")
-            } else if ("Payment cancelled by user." == paymentCancel) {
-                Toast.makeText(this, "Payment cancelled by user.", Toast.LENGTH_SHORT)
-                    .show()
-                Log.e("UPI", "Cancelled by user: $approvalRefNo")
-            } else {
-                Toast.makeText(
-                    this,
-                    "Transaction failed.Please try again",
-                    Toast.LENGTH_SHORT
-                ).show()
-                Log.e("UPI", "failed payment: $approvalRefNo")
-            }
-        } else {
-            Log.e("UPI", "Internet issue: ")
-            Toast.makeText(
-                this,
-                "Internet connection is not available. Please check and try again",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-    }
-*/
 
     private fun getWages(id: String) {
         FirebaseDatabase.getInstance().getReference("users")
@@ -262,7 +156,75 @@ class ConfirmedDateInfoActivity : AppCompatActivity() {
             }
     }
 
-/*
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            UPI_PAYMENT -> {
+                if (resultCode == Activity.RESULT_OK || resultCode == 1) {
+                    var list: java.util.ArrayList<String>
+                    if (data != null) {
+                        list = ArrayList()
+                        data.getStringExtra("resposne")?.let { list.add(it) }
+
+                    } else {
+                        list = ArrayList()
+                        list.add("No Reponse")
+                    }
+                } else {
+                    var list: java.util.ArrayList<String>
+                    list = ArrayList()
+                    list.add("Error")
+
+                    checkPaymentStatus(list)
+                }
+            }
+        }
+    }
+
+    private fun checkPaymentStatus(list: java.util.ArrayList<String>) {
+
+        if (isConnectionAvailable(applicationContext)) {
+            var str = list.get(0);
+            Log.d("UPIPAY", "upiPaymentDataOperation: " + str);
+            if (str == null)
+                str = "discard"
+            var status: String? = null;
+            var txtnNo: String? = null;
+            var paymentCancel: String? = null;
+
+            var response = str.split("&")
+            for (i in 0 until response.size) {
+                val equalStr = response[i].split("=")
+                if (equalStr.size >= 2) {
+                    if (equalStr[0].toLowerCase() == "Status".toLowerCase()) {
+                        status = equalStr[1].toLowerCase()
+                    } else if (equalStr[0].toLowerCase() == "ApprovalRefNo".toLowerCase() || equalStr[0].toLowerCase() == "txnRef".toLowerCase()) {
+                        txtnNo = equalStr[1]
+                    }
+                } else {
+                    paymentCancel = "Payment cancelled by user."
+                }
+            }
+            if (status.equals("success")) {
+                //Code to handle successful transaction here.
+                Toast.makeText(this, "Transaction successful.", Toast.LENGTH_SHORT).show();
+                Log.d("UPI", "responseStr: " + txtnNo);
+            } else if ("Payment cancelled by user.".equals(paymentCancel)) {
+                Toast.makeText(this, "Payment cancelled by user.", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Transaction failed.Please try again", Toast.LENGTH_SHORT)
+                    .show();
+            }
+
+        } else {
+            Toast.makeText(this, "Please check your network", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
     fun isConnectionAvailable(context: Context): Boolean {
         val connectivityManager =
             context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
@@ -277,5 +239,4 @@ class ConfirmedDateInfoActivity : AppCompatActivity() {
         }
         return false
     }
-*/
 }
--- app/src/main/java/com/jayesh/finalyearproject/model/Hires.kt
+++ app/src/main/java/com/jayesh/finalyearproject/model/Hires.kt
@@ -1,4 +1,41 @@
 package com.jayesh.finalyearproject.model
 
 class Hires {
+    var uname: String? = null
+    var uid: String? = null
+    var uDate: String? = null
+    var senderUpi: String? = null
+    var receiverUpi: String? = null
+    var transactionStatus: String? = null
+    var transactionId: String? = null
+    var transactionRefId: String? = null
+    var approvalRefNo: String? = null
+    var amount: String? = null
+
+
+    constructor() {}
+
+    constructor(
+        uname: String,
+        uid: String,
+        uDate: String,
+        senderUpi: String,
+        receiverUpi: String,
+        transactionStatus: String,
+        transactionId: String,
+        transactionRefId: String,
+        approvalRefNo: String,
+        amount: String
+    ) {
+        this.uname = uname
+        this.uid = uid
+        this.uDate = uDate
+        this.senderUpi = senderUpi
+        this.receiverUpi = receiverUpi
+        this.transactionStatus = transactionStatus
+        this.transactionId = transactionId
+        this.transactionRefId = transactionRefId
+        this.approvalRefNo = approvalRefNo
+        this.amount = amount
+    }
 }
